893663efb5e75f4c3ba79f7d0d56f8069be00141
PriyankaRonge <rongepriyanka11@gmail.com>
2020-05-28 18:49:53 +0530

    message

--- HelloWorld/src/main/java/com/infy/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/infy/HelloWorldApplication.java
@@ -9,6 +9,7 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 		System.out.println("first message............");
+		System.out.println("second message ");
 	}
 
 }
